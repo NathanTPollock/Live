10ff41458e44bc89b40ebe51bd61baf10c4699db
NathanTPollock <nathantp132@gmail.com>
2023-04-26 16:41:43 -0600

    Update main.js

--- Project 3/js/main.js
+++ Project 3/js/main.js
@@ -1,1 +1,27 @@
+function goHome() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("content").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+  }
 
+  function goAbout() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("about").style.display = "block";
+    document.getElementById("content").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+  }
+
+  function goContent() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+  }
+
+  function goContact() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("content").style.display = "none";
+    document.getElementById("contact").style.display = "block";
+  }
